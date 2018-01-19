3189f03cbfd37cc7539f046ff6693fe19b31853e
php 10 <php.10@lampadinha.cec.ime.usp.br>
2018-01-19 21:13:35 -0200

    programa exibe 100 x meu nome

---
+++ logica.php
@@ -0,0 +1,34 @@
+<?php
+
+// operadores aritmeticos
+// +, -, *, /
+
+// operadorews logicos
+//and E or
+//&& E ||
+//$a = (true && false);
+//$b = (true and false);
+
+//var_dump($a);
+//var_dump($b);
+
+// operadores de comparação
+//<=>
+//var_dump(2 <=> 1); // 1
+//var_dump(1 <=> 1); // 0
+//var_dump(1 <=> 2); // -1
+
+
+// operadors booleanos
+//<,>,<=,>=, ==, !=
+//=== verifica se é do mesmo tipo, !== ?????????/
+// == e ===
+//var_dump(10 == "10 maça") // isso retorna true
+
+for ($i=0; $i < 10; $i++) {
+echo ($i+1).PHP_EOL;
+}
+
+for ($i=0; $i < 100; $i++) {
+echo ("$i  teodoro "."mendes \n");
+}
